5814f472da1506cbc4e6f21f8351340edcbe3c6e
ishita-1305 <ishsri00@gmail.com>
2025-02-24 13:09:10 +0530

    fix: missing tokensalt api

--- config/admin.ts
+++ config/admin.ts
@@ -1,17 +1,17 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("aGFjnqzqFp+XKJsfyoPQ6A=="),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("JFoUAEHg865w120XsyxG4g==T"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("RS3jzCzgGfWBpY9c4CLaMg=="),
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
   },
 });
